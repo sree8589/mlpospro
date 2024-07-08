daf6c834eb8f437aee3396381386859534ecaa27
mahesh <mahesh@expertzlab.com>
2024-07-08 14:48:01 +0530

    prediction file app.py ceated

---
+++ apps.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import pandas as pandas
+from src.exception.exception import customexception
+from src.logger.my_logging import logging
+
+from src.utils.utils import load_object
+
