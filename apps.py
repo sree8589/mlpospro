abcf0586a161ef6f10d4f8c23ea48a02c3bec237
mahesh <mahesh@expertzlab.com>
2024-07-08 15:49:36 +0530

    app.py created and completed flask deply

--- apps.py
+++
@@ -1,8 +0,0 @@
-import os
-import sys
-import pandas as pandas
-from src.exception.exception import customexception
-from src.logger.my_logging import logging
-
-from src.utils.utils import load_object
-
